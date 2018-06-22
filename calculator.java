4579b7e63f8d8b60374f3fc13f7b634238d46850
pravalikakola <32535182+pravalikakola@users.noreply.github.com>
2018-06-22 07:36:58 +0530

    Update calculator.java

--- calculator.java
+++ calculator.java
@@ -1,5 +1,8 @@
-public class Calculator{
-  int sum(int num1,int num2){
-  return num1+num2;
-  }
+package junit;
+
+public class JUnitHelloWorld {
+
+	public boolean isGreater(int num1, int num2){
+		return num1 > num2;
+	}
 }
